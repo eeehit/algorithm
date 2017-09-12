3f9d2b100edde4f20eb64e3849053b0b45a70d6f
eeehit <eeehit.sytem@gmail.com>
2017-09-12 12:02:04 +0000

    퍼포먼스 코드 수정

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ import lcm
 import performanceCheck
 
 if __name__ == "__main__":
-	n = random.randrange(2, 100)
-	#m = random.randrange(2, n-1)
-	print(performanceCheck.testPrimeNumberIsPrimeBasic(n))
-	print(performanceCheck.testPrimeNumberIsPrimeSqrt(n))
-	print(performanceCheck.testDivisorDivisorBasic(n))
-	print(performanceCheck.testDivisorDivisorSqrt(n))	
-	print(performanceCheck.testGcdEuclideanIteration(100,15))
-	print(performanceCheck.testGcdEuclideanRecursive(100,15))
-	
+	print(performanceCheck.testPrimeNumberIsPrimeBasic())
+	print(performanceCheck.testPrimeNumberIsPrimeSqrt())
+	print(performanceCheck.testDivisorDivisorBasic())
+	print(performanceCheck.testDivisorDivisorSqrt())	
+	print(performanceCheck.testGcdEuclideanRecursive())
+	print(performanceCheck.testGcdEuclideanIteration())
 	print(gcd.euclideanIteration(100,15))
 	print(gcd.euclideanRecursive(100,15))
--- performanceCheck.py
+++ performanceCheck.py
@@ -22,50 +22,58 @@ def average(values):
 def printLog(func, average):
 	return ("[%s]\t%f" % (func, average))
 
-def testPrimeNumberIsPrimeBasic(n):
+def testPrimeNumberIsPrimeBasic():
 	count = []
 	for i in range(1,100):
+		n = random.randrange(2, 100)
 		start_time = start()
 		primeNumber.isPrimeBasic(n)
 		count.append(end(start_time))
 	return printLog("primeNumber.isPrimeBasic", average(count))
 
-def testPrimeNumberIsPrimeSqrt(n):
+def testPrimeNumberIsPrimeSqrt():
 	count = []
 	for i in range(1,100):
+		n = random.randrange(2, 100)
 		start_time = start()
 		primeNumber.isPrimeSqrt(n)
 		count.append(end(start_time))
 	return printLog("primeNumber.isPrimeSqrt", average(count))
 
-def testDivisorDivisorBasic(n):
+def testDivisorDivisorBasic():
 	count = []
 	for i in range(1,100):
+		n = random.randrange(2, 100)
 		start_time = start()
 		divisor.divisorBasic(n)
 		count.append(end(start_time))
 	return printLog("divisor.divisorBasic", average(count))
 
-def testDivisorDivisorSqrt(n):
+def testDivisorDivisorSqrt():
 	count = []
 	for i in range(1,100):
+		n = random.randrange(2, 100)
 		start_time = start()
 		divisor.divisorSqrt(n)
 		count.append(end(start_time))
 	return printLog("divisor.divisorSqrt", average(count))
 
-def testGcdEuclideanIteration(a, b):
+def testGcdEuclideanRecursive():
 	count = []
-	for i in range(1,3000):
-		start_time = start()
-		gcd.euclideanIteration(a, b)
-		count.append(end(start_time))
-	return printLog("gcd.euclideanIteration", average(count))
-
-def testGcdEuclideanRecursive(a, b):
-	count = []
-	for i in range(1,3000):
+	a = random.randrange(2, 100)
+	b = random.randrange(2, a)
+	for i in range(1,100):
 		start_time = start()
 		gcd.euclideanRecursive(a, b)
 		count.append(end(start_time))
 	return printLog("gcd.euclideanRecursive", average(count))
+
+def testGcdEuclideanIteration():
+	count = []
+	a = random.randrange(2, 100)
+	b = random.randrange(2, a)
+	for i in range(1,100):
+		start_time = start()
+		gcd.euclideanIteration(a, b)
+		count.append(end(start_time))
+	return printLog("gcd.euclideanIteration", average(count))
