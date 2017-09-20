440d5b2fdd925ebdf891c9fccd849b7307128a90
eeehit <eeehit@gmail.com>
2017-09-20 15:03:55 +0900

    가운데 글자 가져오기
    https://programmers.co.kr/learn/challenge_codes/83

---
+++ string_middle.py
@@ -0,0 +1,7 @@
+def string_middle(str):
+    # 함수를 완성하세요
+    return str[(len(str)-1)//2 : round((len(str)-1)/2)+1]
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(string_middle("test"))
+print(string_middle("power"))
