0a5d60ebb5258bd3c42443f902b783ec0d424881
eeehit <eeehit@gmail.com>
2017-09-20 17:56:40 +0900

    Create 수박수박수박수박수박수.java

---
+++ 수박수박수박수박수박수.java
@@ -0,0 +1,32 @@
+//수박수박수박수박수박수?
+//Level 1
+//water_melon함수는 정수 n을 매개변수로 입력받습니다.
+//길이가 n이고, 수박수박수...와 같은 패턴을 유지하는 문자열을 리턴하도록 함수를 완성하세요.
+//예를들어 n이 4이면 '수박수박'을 리턴하고 3이라면 '수박수'를 리턴하면 됩니다.
+
+public class WaterMelon {
+	public String watermelon(int n){
+    String w = "수박";
+    String out = "";
+    
+    while (n>0) {
+    	if (n > 1) {
+    		out = out + "수박";
+        n = n-2;
+    	}
+      if (n == 1) {
+        out = out + "수";
+        n = n-1;
+      }
+    }
+    
+		return out;
+	}
+
+	// 실행을 위한 테스트코드입니다.
+	public static void  main(String[] args){
+		WaterMelon wm = new WaterMelon();
+		System.out.println("n이 3인 경우: " + wm.watermelon(3));
+		System.out.println("n이 4인 경우: " + wm.watermelon(4));
+	}
+}
