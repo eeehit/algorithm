889d54e95f27e30e7ebe7a82f104b62524f38f1e
eeehit <eeehit@gmail.com>
2017-09-20 16:24:09 +0900

    Update and rename string_middle.py to 가운데 글자 가져오기.py

--- string_middle.py
+++
@@ -1,7 +0,0 @@
-def string_middle(str):
-    # 함수를 완성하세요
-    return str[(len(str)-1)//2 : round((len(str))/2)+1]
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(string_middle("test"))
-print(string_middle("power"))
