c42ccf893a55fdc4aa23879f6bf5a525bdaa45aa
eeehit <eeehit@gmail.com>
2017-09-21 15:59:20 +0900

    Create 시저 암호.py

---
+++ 시저 암호.py
@@ -0,0 +1,31 @@
+#시저 암호
+#Level 3
+#어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다.
+#A를 3만큼 밀면 D가 되고 z를 1만큼 밀면 a가 됩니다. 공백은 수정하지 않습니다.
+#보낼 문자열 s와 얼마나 밀지 알려주는 n을 입력받아 암호문을 만드는 ceasar 함수를 완성해 보세요.
+#“a B z”,4를 입력받았다면 “e F d”를 리턴합니다.
+
+import string
+
+def caesar(s, n):
+    result = ""
+    lower = string.ascii_lowercase[:26]
+    upper = string.ascii_uppercase[:26]
+    
+    adder = ''
+    for x in s:
+        if x == ' ':
+            adder = ' '
+        elif x.isupper():
+            j = upper.find(x)
+            adder = upper[(j+n)%26]
+        elif x.islower():
+            i = lower.find(x)
+            adder = lower[(i+n)%26]
+        result += adder
+    return result
+    # 주어진 문장을 암호화하여 반환하세요.
+
+
+# 실행을 위한 테스트코드입니다.
+print('s는 "a B z", n은 4인 경우: ' + caesar("a B z", 4))
